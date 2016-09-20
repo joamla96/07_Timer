560e948ae05caf584920e05040d01f7e2c95ae80
jona8690 <jona8690@edu.eal.dk>
2016-09-20 10:00:34 +0200

    Add project files.

---
+++ 07_Timer/Timer.cs
@@ -0,0 +1,13 @@
+﻿namespace _07_Timer {
+	internal class Timer {
+		private int seconds;
+		public int Seconds {
+			get { return this.seconds; }
+			set { this.seconds = value; }
+		}
+
+		public Timer() {
+			this.seconds = 0;
+		}
+	}
+}
