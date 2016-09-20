49a11d921890acdb5b4df64cef463f4d96aa8f6e
jona8690 <jona8690@edu.eal.dk>
2016-09-20 10:20:32 +0200

    /r/didthemath

--- 07_Timer/Timer.cs
+++ 07_Timer/Timer.cs
@@ -1,13 +1,25 @@
 ﻿namespace _07_Timer {
 	internal class Timer {
-		private int seconds;
+		private int seconds = 0;
+		private int minutes = 0;
+		private int hours = 0;
 		public int Seconds {
 			get { return this.seconds; }
 			set { this.seconds = value; }
 		}
 
-		public Timer() {
-			this.seconds = 0;
+		public override string ToString() {
+			while(this.seconds >= 60) {
+				this.seconds -= 60;
+				this.minutes++;
+
+				if(minutes >= 60) {
+					this.minutes -= 60;
+					this.hours++;
+				}
+			}
+
+			return this.hours + ":" + this.minutes + ":" + this.seconds;
 		}
 	}
 }
